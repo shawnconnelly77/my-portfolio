98d52c46bb6bda90591fbe318dcc859f35deb700
Shawn Connelly <shawn.connelly77@gmail.com>
2022-02-23 23:11:10 -0500

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import Main from './MainComponent';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
+class Directory extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Directory'
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
         const renderDirectoryItem = ({item}) => {
-            return(
+            return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    onPress={() => props.onPress(item.id)}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
                     leftAvatar={{ source: require('./images/react-lake.jpg')}}
                 />
             );
         };
-        return(
+
+        return (
             <FlatList
-                data={props.campsites}
+                data={this.state.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
             />
         );
+    }
 }
 
 export default Directory;
